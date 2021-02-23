1c79b0e305ec3ca66984ce8fe2c72ef58e12637c
katiewang22 <katie.wang1950f@gmail.com>
2021-02-23 16:05:30 -0500

    set up robot map

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class RobotMap {
+    // TODO: figure out port #s
+    
+    public static final int LEFT_FRONT_MOTOR = 0;
+    public static final int LEFT_BACK_MOTOR = 0;
+    public static final int RIGHT_FRONT_MOTOR = 0;
+    public static final int RIGHT_BACK_MOTOR = 0;
+
+    public static WPI_TalonFX leftFrontMotor = new WPI_TalonFX(LEFT_FRONT_MOTOR);
+    public static WPI_TalonFX leftBackMotor = new WPI_TalonFX(LEFT_BACK_MOTOR);
+    public static WPI_TalonFX rightFrontMotor = new WPI_TalonFX(RIGHT_FRONT_MOTOR);
+    public static WPI_TalonFX rightBackMotor = new WPI_TalonFX(RIGHT_BACK_MOTOR);
+}
