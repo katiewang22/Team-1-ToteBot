5fb3f729eda4122509eb3bf83feeabada693b2c8
katiewang22 <katie.wang1950f@gmail.com>
2021-02-23 16:23:24 -0500

    drive subsystem vars

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -14,6 +14,18 @@ public class DriveSubsystem {
 
     private static XboxController driverController = RobotContainer.driverController;
 
+    public static final int MOTOR_ENCODER_CODES_PER_REV = 2048;
+    public static final double DIAMETER_INCHES = 5.0;
     private static final double IN_TO_M = .0254;
+
+    public static final double WHEEL_DIAMETER = DIAMETER_INCHES * IN_TO_M;
+    public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+
+    private static final double GEAR_RATIO = 12.75;
+
+    private static final double TICKS_PER_METER = (MOTOR_ENCODER_CODES_PER_REV * GEAR_RATIO) / (WHEEL_CIRCUMFERENCE);
+    private static final double METERS_PER_TICKS = 1/TICKS_PER_METER;
+
+    public boolean state_flag_motion_profile = true;
     
 }
