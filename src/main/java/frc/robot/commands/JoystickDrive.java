32d126acb38292243d865d9f8cbb70bbece1d57f
NannersSaxophone <75700846+NannersSaxophone@users.noreply.github.com>
2021-02-25 16:28:47 -0500

    JoystickDrive

--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -1,5 +1,18 @@
 package frc.robot.commands;
 
-public class JoystickDrive {
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveSubsystem;
 
+public class JoystickDrive extends CommandBase{
+    private final DriveSubsystem driveSubsystem;
+    private final static XboxController driverController = RobotContainer.driverController;
+
+    public JoystickDrive(DriveSubsystem drivetrain) {
+        driveSubsystem = drivetrain;
+        addRequirements(driveSubsystem);
+    }
+
+    
 }
