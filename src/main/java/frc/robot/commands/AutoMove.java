5804eaf3bd861850e7ef1aff63e9199f3e6ee902
katiewang22 <katie.wang1950f@gmail.com>
2021-03-04 16:09:04 -0500

    clean up

--- src/main/java/frc/robot/commands/AutoMove.java
+++ src/main/java/frc/robot/commands/AutoMove.java
@@ -8,55 +8,40 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoMove extends CommandBase {
-  private final DriveSubsystem driveSubsystem;
-  private double start_dist_meters;
-  private double target_distance;
-  /** Creates a new AutoMove. */
+  private final DriveSubsystem driveSubsystem; // Create an instance of the Drive Subsystem
+  private double START_DIST_METERS; // Distance travelled at start of match
+  private double TARGET_DISTANCE; // Target distance to travel during match
 
-  public AutoMove(final DriveSubsystem subsystem, Mode mode, double distance_in_meters, double speed) {
-    // Use addRequirements() here to declare subsystem dependencies.
+  // Constructor initializes DriveSubsystem and distance in meters
+  public AutoMove(DriveSubsystem subsystem, double DISTANCE_IN_METERS) {
     driveSubsystem = subsystem;
     addRequirements(driveSubsystem);
-    start_dist_meters = driveSubsystem.distanceTravelledInMeters();
-    target_distance = distance_in_meters;
-  }
-
-  public AutoMove(final DriveSubsystem subsystem, double distance) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    driveSubsystem = subsystem;
-    addRequirements(driveSubsystem);
-    start_dist_meters = driveSubsystem.distanceTravelledInMeters();
-    target_distance = distance;
+    START_DIST_METERS = driveSubsystem.distanceTravelledInMeters();
+    TARGET_DISTANCE = DISTANCE_IN_METERS;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    driveSubsystem.resetEncoders();
-    driveSubsystem.ZeroYaw();
-    start_dist_meters = driveSubsystem.distanceTravelledInMeters();
-    driveSubsystem.state_flag_motion_profile = true;
+    driveSubsystem.resetEncoders(); // Resets encoders
+    driveSubsystem.ZeroYaw(); // Sets Yaw to 0
+    START_DIST_METERS = driveSubsystem.distanceTravelledInMeters(); // Sets starting distance to distance travelled
+    driveSubsystem.state_flag_motion_profile = true; // starts motion profiling
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveSubsystem.drive(target_distance, 0.0);
+    DriveSubsystem.drive(TARGET_DISTANCE, 0.0);
   }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    double distance_driven = driveSubsystem.distanceTravelledInMeters() - start_dist_meters;
-    double positionError = Math.abs(target_distance = distance_driven);
-    return(positionError < 0.01);
-  }
+    // Checks how accurately we moved by getting the difference between distance driven and target distance
+    double distanceDriven = driveSubsystem.distanceTravelledInMeters() - START_DIST_METERS;
+    double positionError = Math.abs(TARGET_DISTANCE - distanceDriven);
 
-  public enum Mode {
-    DISTANCE, TIME
+    return(positionError < 0.01);
   }
 }
