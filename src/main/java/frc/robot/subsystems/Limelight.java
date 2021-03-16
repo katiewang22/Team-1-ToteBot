4596dba23f966b8c50f5cfddd63f0bb85750fa2b
katiewang22 <katie.wang1950f@gmail.com>
2021-03-16 16:23:40 -0400

    limelight vars

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,11 +4,26 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
-  /** Creates a new Limelight. */
-  public Limelight() {}
+
+  public static NetworkTable limelightTable;
+
+  public static double limelight_x;
+  public static double limelight_y;
+
+  public static int FORCE_OFF = 1;
+  public static int FORCE_BLINK = 2;
+  public static int FORCE_ON = 3;
+
+  public static int VISION_PROCESSOR = 0;
+  public static int DRIVER_CAMERA = 1;
+  
+  public Limelight() {
+
+  }
 
   @Override
   public void periodic() {
